f38444b1b34059e08a640bc537e7fa1c19141b38
Asfaw Nahom <s2200108@edu.bc.fi>
2022-05-18 12:13:42 +0300

    functionality added

--- web/src/Controller/MainController.php
+++ web/src/Controller/MainController.php
@@ -3,14 +3,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Answer;
 
 
 class MainController extends AbstractController {
 
     #[Route('/', name:'home')]
-    public function home(): Response
-    {
+    public function home(EntityManagerInterface $em): Response
+    {   
+
+        $answers = $em->getRepository(Answer::class)->findAll();
+        $data = [];
+        foreach ($answers as $answer) {
+            $data[] = [
+                //'id' => $answer->getId(),
+                'name' => $answer->getAnswer()
+             
+            ];
+        }
+
         
+        
+        //dump($this->json($data[2]));
         return $this->render('html/home.html.twig');
     }
 
@@ -23,11 +38,53 @@ class MainController extends AbstractController {
 
 
     #[Route('/classroom', name:'classroom')]
-    public function classRoom(): Response
+    public function answer(): Response
     {
         
-        return $this->render('html/classroom.html.twig');
+
+        function randAns () {
+
+            $array = [
+
+                "no questions, ok lets continue",
+                
+                "people in the remote are you there?",
+                
+                "if you get an error is good!",
+                
+                "...and thats the whole idea!",
+                
+                "ok, give it a try!",
+                
+                "who, is reading next?"];
+           return $array[array_rand($array)];
+        }
+
+        function randQus () {
+
+            $arrayQ = [
+
+                "What means symfony?",
+                
+                "Why we need PHP?",
+                
+                "What is twig for?",
+                
+                "Can you give more time for the assignment?",
+                
+                "Can you be slower please?",
+                
+                "Screen share plaese?"];
+           return $arrayQ[array_rand($arrayQ)];
+        }
+            
+        
+        return $this->render('html/classroom.html.twig',[
+            'answer' => randAns(),'question' => randQus()
+          ]);
     }
+
+    
 
 }
 
